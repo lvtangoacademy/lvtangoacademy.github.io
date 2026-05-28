1a7738afed846de39e1e7a7fc281e697c5461ba7
hj.lee <hj.lee@fullcontact.com>
2026-05-28 01:00:33 -0700

    removed dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+    base: '/lvtangoacademy.github.io/'
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
